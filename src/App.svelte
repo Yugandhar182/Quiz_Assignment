9a63eff28b4d64aa70765dacf8031608522858fb
Yugandhar182 <yugandhar@recruitly.io>
2023-05-22 15:04:55 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,3 @@
-
-<h1 style="color:red;"> Answer the following the quiz</h1>
 <script>
 	import { onMount } from "svelte";
 	import 'bootstrap/dist/css/bootstrap.min.css';
@@ -39,7 +37,13 @@
   
 	let score = 0;
 	let currentQuestion = 0;
+	let isQuizStarted = false;
 	let isQuizFinished = false;
+  
+	let firstName = "";
+	let lastName = "";
+	let email = "";
+	let mobile = "";
   
 	function selectOption(option) {
 	  questions[currentQuestion].selected = option;
@@ -52,7 +56,7 @@
 	  if (currentAnswer === correctAnswer) {
 		score++;
 	  }
-	  
+  
 	  if (currentQuestion < questions.length - 1) {
 		currentQuestion++;
 	  } else {
@@ -60,9 +64,18 @@
 	  }
 	}
   
+	function startQuiz() {
+	  if (firstName && lastName && email && mobile) {
+		isQuizStarted = true;
+	  } else {
+		alert("Please fill in all the details.");
+	  }
+	}
+  
 	onMount(() => {
 	  score = 0;
 	  currentQuestion = 0;
+	  isQuizStarted = false;
 	  isQuizFinished = false;
   
 	  questions.forEach((question) => {
@@ -71,15 +84,38 @@
 	});
   </script>
   
+  <style>
+	li {
+	  cursor: pointer;
+	}
+  </style>
+  
   <main class="container mt-4">
-	{#if isQuizFinished}
-	  <div class="card">
-		<div class="card-body">
-		  <h1 class="card-title">Quiz Finished</h1>
-		  <p class="card-text">Your score: {score} out of {questions.length}</p>
+	{#if !isQuizStarted}
+	  {#if !firstName || !lastName || !email || !mobile}
+		<h1 style="color:red;">Please fill the form before starting the quiz</h1>
+	  {/if}
+	  <form on:submit|preventDefault={startQuiz}>
+		<div class="mb-3">
+		  <label for="firstName" class="form-label">First Name</label>
+		  <input type="text" class="form-control" id="firstName" bind:value={firstName} required>
 		</div>
-	  </div>
-	{:else}
+		<div class="mb-3">
+		  <label for="lastName" class="form-label">Last Name</label>
+		  <input type="text" class="form-control" id="lastName" bind:value={lastName} required>
+		</div>
+		<div class="mb-3">
+		  <label for="email" class="form-label">Email</label>
+		  <input type="email" class="form-control" id="email" bind:value={email} required>
+		</div>
+		<div class="mb-3">
+		  <label for="mobile" class="form-label">Mobile Number</label>
+		  <input type="tel" class="form-control" id="mobile" bind:value={mobile} required>
+		</div>
+		<button type="submit" class="btn btn-primary">Start Quiz</button>
+	  </form>
+	{:else if !isQuizFinished}
+	  <h1 style="color:red;">Answer the following quiz</h1>
 	  <div class="card">
 		<div class="card-body">
 		  <h1 class="card-title">Question {currentQuestion + 1}</h1>
@@ -105,12 +141,12 @@
 		  <button class="btn btn-primary mt-3" on:click={submitAnswer}>Next</button>
 		</div>
 	  </div>
+	{:else}
+	  <div class="card">
+		<div class="card-body">
+		  <h1 class="card-title">Quiz Finished</h1>
+		  <p class="card-text">Your score: {score} out of {questions.length}</p>
+		</div>
+	  </div>
 	{/if}
   </main>
-  
-  <style>
-	li {
-	  cursor: pointer;
-	}
-  </style>
-  
